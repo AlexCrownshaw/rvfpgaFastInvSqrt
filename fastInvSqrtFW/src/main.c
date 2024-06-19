c7b996c703867fa26dddb7e39ab194551e63b65a
Alex Crownshaw <alex.crownshaw12@gmail.com>
2024-06-19 19:43:16 +0100

    Initial testing of wishbone read operation

--- fastInvSqrtFW/src/main.c
+++ fastInvSqrtFW/src/main.c
@@ -1,12 +1,26 @@
 #include <stdint.h>
+#include <bsp_printf.h>
+#include <bsp_mem_map.h>
+#include <bsp_version.h>
+#include <psp_api.h>
+
 
 #define WRITE_ADDR(dir, value) { (*(volatile unsigned int*)dir) = (value); }
+#define READ_ADDR(dir) (*(volatile unsigned int*)dir)
+
 #define FAST_INV_SQRT_INPUT_ADDR 0x80003100
+#define FAST_INV_SQRT_OUTPUT_ADDR 0x80003102
+
 
 int main(void)
 {
 	unsigned int x = 24; // 1.5 in q12.4 (0000_0000_0001_1000)
 	WRITE_ADDR(FAST_INV_SQRT_INPUT_ADDR, x);
+
+	for (int i=0;i<10000000;i++){};	// Delay
+
+	unsigned int y = READ_ADDR(FAST_INV_SQRT_OUTPUT_ADDR);
+	printfNexys("y: %d", y);
 
 	return 0;
 }
